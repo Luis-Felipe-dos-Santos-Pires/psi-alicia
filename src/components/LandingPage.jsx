2dbbf4766dfbb47e99bbb9ce80fae13e26d94226
Luis-Felipe-dos-Santos-Pires <lfpires04@outlook.com>
2024-01-29 19:41:28 -0300

    Abstracting the menu component to just contain the menu

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import Menu from './Menu'
+import Avatar from "../assets/alicia.png";
 
 function LandingPage () {
     return (
         <div class="w-screen h-full bg-landing-page bg-cover bg-no-repeat bg-center">
-            <div class="h-full w-full gap-0 columns-2">
+            <Menu />
+            <div class="h-4/5 w-full gap-0 columns-2">
                 <div class="h-full w-full flex flex-col justify-center items-center">
                     <text class="text-7xl font-serif text-slate-300">Alicia Alves</text>
                     <text class="text-xl font-serif text-slate-300">Psicóloga - CRP 123456</text>
                 </div>
                 <div class="h-full w-full">
-                    <Menu />
+                    <div class="h-4/5 w-full flex items-center justify-center">
+                        <img class="w-96 h-96 rounded-full" src={Avatar}></img>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,17 +9,12 @@ function Item({label, link, twclass}) {
 
 function Menu () {
     return (
-        <div class="h-full w-full gap-0 m-0 p-0 flex-col">
-            <div class="h-20 self-start gap-0 columns-5">
-                <Item label="Sobre mim" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
-                <Item label="Abordagem" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
-                <Item label="Contato" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
-                <Item label="Código de ética" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
-                <Item label="Insta" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
-            </div>
-            <div class="h-4/5 w-full flex items-center justify-center">
-                <img class="w-96 h-96 rounded-full" src={Avatar}></img>
-            </div>
+        <div class="h-1/5 self-start gap-0 columns-5">
+            <Item label="Sobre mim" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
+            <Item label="Abordagem" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
+            <Item label="Contato" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
+            <Item label="Código de ética" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
+            <Item label="Insta" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
         </div>
     )
 }
