8dc49653d2d092534f93834583d506463b0c8aa7
Luis-Felipe-dos-Santos-Pires <lfpires04@outlook.com>
2024-01-29 21:31:49 -0300

    Adding some navigation to the site

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import Avatar from "../assets/alicia.png";
+import {Link} from "react-router-dom";
+
 
 function Item({label, link, twclass}) {
     return <div class="h-full flex justify-center items-center">
-        <text href={link} class={twclass}>{label}</text>
+        <Link to={link} class={twclass}>{label}</Link>
     </div>
 }
 
 function Menu () {
     return (
         <div class="h-full w-full self-start gap-0 columns-4">
-            <Item label="Sobre mim" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
+            <Item label="Sobre mim" link="#AboutMe" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
             <Item label="Abordagem" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
             <Item label="Contato" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
             <Item label="Código de ética" link="TESTE" twclass="font-serif text-slate-300 hover:text-slate-100 hover:cursor-pointer" />
