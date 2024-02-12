9e17d1a3c76b39f65aff9a1eaa1361a0b39e9bbf
Luis-Felipe-dos-Santos-Pires <lfpires04@outlook.com>
2024-02-12 19:48:31 -0300

    Migrating pages to use the DefaultComponent

--- src/components/DefaultPage.jsx
+++ src/components/DefaultPage.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import AboutMeImg from "../assets/Pawel-about-me.jpg"
 
-function AboutMe ({Id, BgColor, Image, ImageStyle, Title, TitleStyle, Text, TextStyle}) {
+function AboutMe ({Id, BgColor, Image, ImageStyle, Title, TitleStyle, Text, TextStyle, TextOrder}) {
     return (
-        <div id={Id} className={`w-screen h-full ${BgColor} gap-0 columns-2`}>
-            <div className="h-full w-full flex items-center justify-center">
+        <div id={Id} className={`w-screen h-full ${BgColor} gap-0 flex`}>
+            <div className="flex-1 flex items-center justify-center">
                 <img className={ImageStyle} src={Image} />
             </div>
-            <div className="h-full w-full flex flex-col items-center justify-center">
+            <div className={`flex-1 flex flex-col ${TextOrder} items-center justify-center`}>
                 <p className={TitleStyle}>{Title}</p>
                 <div className="w-4/6">
                     <p className={TextStyle}>{Text}</p>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Menu from './Menu'
 import Avatar from "../assets/alicia.png";
+import DefaultPage from './DefaultPage'
 
 function LandingPage () {
     return (
@@ -9,14 +10,8 @@ function LandingPage () {
                 <div className="h-full w-full"></div>
                 <Menu />
             </div>
-            <div className="h-4/5 w-full gap-0 columns-2">
-                <div className="h-full w-full flex flex-col justify-center items-start pl-48">
-                    <p className="text-7xl font-serif text-slate-300">Alicia Alves</p>
-                    <p className="text-xl font-serif text-slate-300">Psicóloga - CRP 123456</p>
-                </div>
-                <div className="h-4/5 w-fullm flex items-center justify-start pl-64">
-                    <img className="w-96 h-96 rounded-full" src={Avatar}></img>
-                </div>
+            <div className="h-4/5 w-full">
+                <DefaultPage Id="" Image={Avatar} BgColor="bg-transparent" ImageStyle="w-96 h-96 rounded-full" Title="Alicia Alves" TitleStyle="text-7xl font-serif text-slate-300" Text="Psicóloga - CRP 123456" TextOrder="order-first" TextStyle="text-xl font-serif text-slate-300"/>
             </div>
         </div>
     )
