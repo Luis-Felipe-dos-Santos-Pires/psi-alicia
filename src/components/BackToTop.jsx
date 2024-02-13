a3a7f4eff4719d74491cc25f940b367a3e4d9f4f
Luis-Felipe-dos-Santos-Pires <lfpires04@outlook.com>
2024-02-13 20:00:36 -0300

    Adding reactivity to the scroll to top button

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -3,12 +3,12 @@ import {HashLink as Link} from "react-router-hash-link";
 import { BsArrowUpCircle } from "react-icons/bs";
 import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
 
-function BackToTop () {
-    return (
-        <button>
-            <Link to="#LandingPage" smooth={true} replace={true} className="fixed bottom-20 right-10 p-1 z-40 rounded-full bg-transparent">{<MdOutlineKeyboardDoubleArrowUp color="#FFF" size="30px"/>}</Link>
-        </button>
-    )
+function BackToTop ({scrollTop}) {
+    if (scrollTop > 100) {
+        return <Link to="#LandingPage" smooth={true} replace={true} className="fixed bottom-20 right-10 p-1 z-40 rounded-full bg-transparent">{<MdOutlineKeyboardDoubleArrowUp color="#f1f5f9" size="35px"/>}</Link>
+    } else {
+        return <></>
+    }
 }
 
 export default BackToTop
